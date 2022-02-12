eba36f4d8a55c011e3c79166616b19cdb9fad98e
Muhammad Hasham <hasham25525@gmail.com>
2022-02-13 00:47:30 +0500

    first commit

---
+++ js/base.js
@@ -0,0 +1,20 @@
+// // number
+// let num = 2;
+// document.write("hehe");
+
+// // string
+// let str = 'hello friends'
+
+// // objects
+// let marks = {
+//     hasham: 88,
+//     anas: 79,
+//     bilal: 78
+// }
+
+let arr = [22, 33, 44, 55, 66, 77];
+
+arr.forEach(element => console.log(element));                 //element => == function(element)
+
+
+
