33b712d5c7a323b620d7dc87900dc87ff7f01cb2
Muhammad Hasham <hasham25525@gmail.com>
2022-02-20 02:12:34 +0500

    some methods

--- js/base.js
+++ js/base.js
@@ -14,8 +14,20 @@
 
 let arr = [22, 33, 44, 55, 66, 77];
 
-arr.forEach(element => console.log(element));                 //element => == function(element)
-console.log(arr);
+// arr.forEach(element => console.log(element));                 //element => == function(element)
+//  array functions
+// arr.pop()
+// arr.push()
+// arr.shift()
+// arr.unshift()
 
 
+// string function
+// let myStr = "My name is Hasham";
+// console.log(myStr.indexOf("name"));
+// myStr.slice(0, 10);
+// myStr.replace("Hasham", "Anas")
 
+// Date
+let myDate = new Date();
+console.log(myDate.getDate());
