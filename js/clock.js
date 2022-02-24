8c0f8f8d5b22ebe52901793ae00e8a5b760e4215
Muhammad Hasham <hasham25525@gmail.com>
2022-02-24 18:22:17 +0500

    woking clock

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,21 @@
-let clock = new Date();
-let hr = clock.getHours();
-let min = clock.getMinutes();
-let sec = clock.getSeconds();
+function MyCLock() {
+    let clock = new Date();
+    let hr = clock.getHours();
+    let min = clock.getMinutes();
+    let sec = clock.getSeconds();
+    let a = "AM";
+    if (hr == 0) {
+        hr = 12;
+    }
+    if (hr > 12) {
+        hr = hr - 12;
+        a = "PM";
+    }
 
-let time = hr + ":" + min + ":" + sec;
-document.getElementById("clock").innerText = time;
 
+    let time = hr + ":" + min + ":" + sec + " " + a;
+    document.getElementById("clock").innerText = time;
+    setTimeout(MyCLock, 1000);
+}
+
+MyCLock();
